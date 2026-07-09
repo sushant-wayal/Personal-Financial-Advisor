111b4d3eb6b72b18cb0a3a8a81a9c66c4fc07334
Sushant Wayal <sushantssr12345@gmail.com>
2026-07-09 23:53:00 +0530

    fix: reverted test base url change

--- mobile/src/lib/apiBaseUrl.ts
+++ mobile/src/lib/apiBaseUrl.ts
@@ -1,5 +1,5 @@
-// const normalizedApiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ?? "https://movenorth.vercel.app";
+const normalizedApiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ?? "https://movenorth.vercel.app";
 
-// export const API_BASE_URL = normalizedApiBaseUrl;
-export const API_BASE_URL = "http://192.168.1.75:3000";
+export const API_BASE_URL = normalizedApiBaseUrl;
+// export const API_BASE_URL = "http://192.168.1.75:3000";
 
